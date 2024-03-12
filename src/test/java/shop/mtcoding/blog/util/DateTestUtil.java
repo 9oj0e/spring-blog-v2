3bbdfcd59eeac11b6bc5eb4166a1721ab927f9d7
JHJO <war2290@gmail.com>
2024-03-12 12:49:13 +0900

    TimeFormatter modularization + jUnit Test

---
+++ src/test/java/shop/mtcoding/blog/util/DateTestUtil.java
@@ -0,0 +1,21 @@
+package shop.mtcoding.blog.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+
+public class DateTestUtil {
+
+    @Test
+    public void timestampFormat_test(){
+        // given
+        Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+
+        // when
+        String createdAt = MyDateUtil.timestampFormat(currentTimestamp);
+
+        // then
+        System.out.println("timestampFormat_test : " + createdAt);
+    }
+
+}
